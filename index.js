57adafd50556ed908439686cf35251e23d3d7ff0
Geet Patel <geetpofficial@gmail.com>
2023-09-17 18:31:16 -0400

    Adding pre-commit & prettier

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const http = require('http');
+const http = require("http");
 
-const hostname = '0.0.0.0';
+const hostname = "0.0.0.0";
 const port = 3000;
 
 const server = http.createServer(async (req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
