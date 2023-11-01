bad3ee4821cd993dcb60adaab6a34c35ccaffc62
hasnaingandhi <hasnain2804@gmail.com>
2023-11-01 18:28:44 -0400

    Updated code with the feedback and added a few validations.

--- backend/routes/booklisting.js
+++ backend/routes/booklisting.js
@@ -3,10 +3,15 @@ const Router = require("express-promise-router");
 const express = require('express');
 const router = express.Router();
 
+//Registered User adding their listings
 router.post('/createListing', async (req, res) => {
+
     const newListing = await listingService.createNewListing(req.body);
-    res.status(200);
-    res.end(JSON.stringify(newListing));
+    if (newListing) {
+        res.status(200).json({ "Request Raised": "True" }); // Status code 200 for success
+    } else {
+        res.status(400).json({ "Request Raised": "False" }); // Status code 400 for bad request
+    }
 });
 
 module.exports = router;
--- backend/services/booklisting.js
+++ backend/services/booklisting.js
@@ -1,10 +1,22 @@
 const db = require("../db");
 async function createNewListing(bookListingData) {
-    const result = await db.query(
-        'INSERT INTO book_listing(owner_id, book_id, status, status_code, returns_on) VALUES($1, $2, $3, $4, NOW()) RETURNING *',
-        [bookListingData.owner_id, bookListingData.book_id, bookListingData.status, bookListingData.status_code]
-    );
-    return result.rows[0];
+    try{
+        const result = await db.query(
+            'INSERT INTO book_listing(owner_id, book_id, status, status_code, returns_on) VALUES($1, $2, $3, $4, NOW()) RETURNING *',
+            [bookListingData.owner_id, bookListingData.book_id, bookListingData.status, bookListingData.status_code]
+        );
+        if (result.rowCount === 1) {
+            console.log("Request created successfully.");
+            return true;
+        } else {
+            console.log("Failed to create request.");
+            return false;
+        }
+    }
+    catch(error){
+        console.error("Error creating request:", error);
+        return false;
+    }
 }
 
 
