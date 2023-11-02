c902b12c5eea81785ad9c454e2b195835ea804e6
MansiGit <mrborole@gmail.com>
2023-11-02 00:50:21 -0400

    added Stripe card add and retrieve

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -18,7 +18,7 @@ router.get("/test", async (req, res) => {
     res.status(200).send("Test working");
 });
 
-router.post('/save-card', async (req, res) => {
+router.post('/saveCard', async (req, res) => {
     try {
       const { email, card } = req.body;
   
@@ -38,76 +38,41 @@ router.post('/save-card', async (req, res) => {
     }
   });
 
-  router.get('/get-cards/:customerId', async (req, res) => {
-    try {
-      const { customerId } = req.params;
-  
-      // Retrieve the customer's saved cards
-      const cards = await stripe.customers.listSources(
-        customerId,
-        { object: 'card', limit: 10 }
-      );
-  
-      // Format and send the card information
-      const formattedCards = cards.data.map(card => ({
-        last4: card.last4,
-        brand: card.brand,
-        exp_month: card.exp_month,
-        exp_year: card.exp_year,
-      }));
-  
-      res.json(formattedCards);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
-  
-router.post('/create-payment-intent', async (req, res) => {
-  try {
-    const { amount, currency } = req.body;
-    const paymentIntent = await paymentService.createPaymentIntent(amount, currency);
-    res.json({ clientSecret: paymentIntent.client_secret });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Internal Server Error');
-  }
+router.get('/getCards/:customerId', async (req, res) => {
+
+try {
+  const { customerId } = req.params;
+
+  // Retrieve the customer's saved cards
+  const cards = await stripe.customers.listSources(
+    customerId,
+    { object: 'card', limit: 10 }
+  );
+
+  // Format and send the card information
+  const formattedCards = cards.data.map(card => ({
+    last4: card.last4,
+    brand: card.brand,
+    exp_month: card.exp_month,
+    exp_year: card.exp_year,
+  }));
+
+  res.json(formattedCards);
+} catch (error) {
+  console.error(error);
+  res.status(500).send('Internal Server Error');
+}
 });
 
-
-// // Borrower requests an exchange
-// router.post("/addCard", async (req, res) => {
-//     try {
-//         const borrowerId = req.body.borrowerId;
-//         const borrowDuration = req.body.borrowDuration;
-//         const listingId = req.body.listingId;
-
-//         if(borrowerId === undefined || borrowDuration === undefined || listingId == undefined){
-//             res.status(400).json({ "Request Raised": "Fail", "Failure Reason": "Invalid Parameters"}); // Status code 400 for bad request
-//         }
-//         else{
-//             // Retrieve lender_id using the getLenderIdByListingId function
-//             const lenderId = await requestService.getLenderIdByListingId(listingId);
-//             console.log(lenderId)
-//             if (lenderId !== null) {
-//                 // Now, you have the lender_id, you can proceed to create the request
-//                 const result = await requestService.raiseBorrowRequest(borrowerId, lenderId, listingId, borrowDuration);
-
-//                 if (result) {
-//                     res.status(200).json({ "Request Raised": "Success" }); // Status code 200 for success
-//                 } else {
-//                     res.status(400).json({ "Request Raised": "Fail", "Failure Reason": "Failed to create request due to server"}); // Status code 400 for bad request
-//                 }
-//             } else {
-//                 res.status(404).json({ "Request Raised": "Listing not found" }); // Status code 404 for not found
-//             }
-//         }
-
-//     } catch (error) {
-//         console.error("Error while creating borrow request:", error);
-//         res.status(500).json({ "Error": "Internal Server Error" }); // Status code 500 for internal server error
-//     }
-// });
-
+router.post('/createPaymentIntent', async (req, res) => {
+try {
+const { amount, currency } = req.body;
+const paymentIntent = await paymentService.createPaymentIntent(amount, currency);
+res.json({ clientSecret: paymentIntent.client_secret });
+} catch (error) {
+console.error(error);
+res.status(500).send('Internal Server Error');
+}
+});
 
 module.exports = router;
--- backend/services/payment.js
+++ backend/services/payment.js
@@ -22,30 +22,5 @@ async function createPaymentIntent(amount, currency) {
   return paymentIntent;
 }
 
-module.exports = { createPaymentIntent };
 
-// async function addCard(userId, , listingId, borrowDuration) {
-//     try {
-//         // Use placeholders to prevent SQL injection
-//         const query = `
-//             INSERT INTO request (borrower_id, lender_id, book_listing_id, time_to_live, status)
-//             VALUES ($1, $2, $3, NOW() + INTERVAL '2 days', 'Pending', 0)
-//         `;
-        
-//         const values = [borrowerId, lenderId, listingId];
-//         console.log("dfdfsfd")
-//         // Execute the query
-//         const result = await db.query(query, values);
-//         console.log(query)
-//         if (result.rowCount === 1) {
-//             console.log("Request created successfully.");
-//             return true;
-//         } else {
-//             console.log("Failed to create request.");
-//             return false;
-//         }
-//     } catch (error) {
-//         console.error("Error creating request:", error);
-//         throw error; // Re-throw the error to handle it at a higher level if needed.
-//     }
-// }
+module.exports = { createPaymentIntent };
