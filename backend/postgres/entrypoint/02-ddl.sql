37d0f064d6bb789fbfdf913bd586e3072c331021
hasnaingandhi <hasnain2804@gmail.com>
2023-11-02 12:45:03 -0400

    DDLs are updated

--- backend/postgres/entrypoint/02-ddl.sql
+++ backend/postgres/entrypoint/02-ddl.sql
@@ -1,114 +1,74 @@
--- Member Table
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    user_id TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
-    email TEXT NOT NULL UNIQUE,
-    phone_number TEXT UNIQUE,
-    first_name TEXT NOT NULL,
-    last_name TEXT NOT NULL,
-    latitude DECIMAL(10, 6),
-    longitude DECIMAL(10, 6),
-    is_auth BOOLEAN DEFAULT FALSE
-);
+-- Users
+INSERT INTO users (user_id, password_hash, email, first_name, last_name)
+VALUES 
+('user1', 'hashed_pass1', 'user1@email.com', 'User', 'One'),
+('user2', 'hashed_pass2', 'user2@email.com', 'User', 'Two'),
+('user3', 'hashed_pass3', 'user3@email.com', 'User', 'Three'),
+('user4', 'hashed_pass4', 'user4@email.com', 'User', 'Four');
 
--- Book Table
-CREATE TABLE book (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    title TEXT NOT NULL,
-    author TEXT NOT NULL,
-    rating DECIMAL(3,2),
-    image_url TEXT NOT NULL
-);
-
--- Genre Table
-CREATE TABLE genre (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    name TEXT NOT NULL UNIQUE
-);
-
--- BookListing Table
-CREATE TABLE book_listing (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    owner_id INTEGER REFERENCES users(id),
-    book_id INTEGER REFERENCES book(id),
-    status TEXT,
-    status_code INTEGER,
-    returns_on DATE
-);
-
--- BookGenreMapping Table
-CREATE TABLE book_genre_mapping (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    book_id INTEGER REFERENCES book(id),
-    genre_id INTEGER REFERENCES genre(id)
-);
-
--- Session Table
-CREATE TABLE session (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    user_id INTEGER REFERENCES users(id),
-    time_to_live timestamp without time zone
-);
-
--- Create an enumerated type for request statuses
-CREATE TYPE request_status AS ENUM ('Pending', 'Accepted', 'Rejected', 'Expired');
+-- Book
+INSERT INTO book (title, author, rating, image_url)
+VALUES
+    ('Book 1', 'Author 1', 4.5, 'https://example.com/image1.jpg'),
+    ('Book 2', 'Author 2', 3.8, 'https://example.com/image2.jpg'),
+    ('Book 3', 'Author 3', 4.2, 'https://example.com/image3.jpg'),
+    ('Book 4', 'Author 4', 4.0, 'https://example.com/image4.jpg'),
+    ('Book 5', 'Author 5', 4.8, 'https://example.com/image5.jpg'),
+    ('Book 6', 'Author 5', 4.8, 'https://example.com/image5.jpg'),
+    ('Book 7', 'Author 5', 4.8, 'https://example.com/image5.jpg'),
+    ('Book 8', 'Author 5', 4.8, 'https://example.com/image5.jpg');
 
 
--- Request Table
-CREATE TABLE request (
-    id SERIAL PRIMARY KEY,
-    created timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_modified timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    borrower_id INTEGER REFERENCES users(id),
-    lender_id INTEGER REFERENCES users(id),
-    book_listing_id INTEGER REFERENCES book_listing(id),
-    time_to_live timestamp without time zone,
-    status request_status
-);
+-- Genre
+INSERT INTO genre (name)
+VALUES 
+('Fiction'),
+('Non-Fiction'),
+('Romance'),
+('Thriller');
 
--- ExchangeHistory Table
-CREATE TABLE exchange_history (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    lender_id INTEGER REFERENCES users(id),
-    borrower_id INTEGER REFERENCES users(id),
-    book_id INTEGER REFERENCES book(id),
-    request_id INTEGER REFERENCES request(id)
-); 
+-- BookListing
+INSERT INTO book_listing (owner_id, book_id, status, status_code)
+VALUES 
+(1, 1, 'Available', 1), -- 1
+(1, 2, 'Available', 1), -- 2
+(2, 3, 'Available', 1), -- 3
+(2, 4, 'Available', 1), -- 4
+(3, 5, 'Available', 1), -- 5
+(3, 6, 'Available', 1), -- 6
+(4, 7, 'Available', 1), -- 7
+(4, 8, 'Available', 1); -- 8
 
--- Payment Table
-CREATE TABLE payment (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    req_id INTEGER REFERENCES request(id),
-    amount DECIMAL(10, 2),
-    txn_id TEXT UNIQUE,
-    payment_status TEXT
-);
+-- BookGenreMapping
+INSERT INTO book_genre_mapping (book_id, genre_id)
+VALUES 
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4);
 
--- -- Cards Tables
--- CREATE TABLE cards(
---     id SERIAL PRIMARY KEY,
---     created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
---     last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
---     payer_id INTEGER REFERENCES users(id)
---     card_number 
---     expiration
---     cvv
+-- Session
+INSERT INTO session (user_id, time_to_live)
+VALUES 
+(1, '2024-01-01 12:00:00'),
+(2, '2024-01-02 12:00:00'),
+(3, '2024-01-03 12:00:00'),
+(4, '2024-01-04 12:00:00');
 
--- );
+-- Request
+INSERT INTO request (borrower_id, lender_id, book_listing_id, time_to_live, status)
+VALUES 
+(1, 2, 3, '2023-01-05 12:00:00', 'Expired'),
+(1, 3, 5, '2024-01-07 12:00:00', 'Pending'),
+(1, 4, 7, '2024-01-07 12:00:00', 'Rejected'),
+(1, 2, 4, '2024-01-07 12:00:00', 'Accepted'),
+(3, 4, 8, '2023-01-06 12:00:00', 'Pending'), 
+(4, 1, 1, '2024-01-07 12:00:00', 'Pending'),
+(1, 4, 1, '2024-01-07 12:00:00', 'Accepted');
+
+-- Payment
+INSERT INTO payment (req_id, amount, txn_id, payment_status)
+VALUES 
+(1, 10.00, 'txn12345', 'Completed'),
+(2, 15.00, 'txn67890', 'Completed'),
+(3, 12.50, 'txn11121', 'Pending');
