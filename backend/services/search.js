f28f91afc4338bd6238a1b314616131ce39386f3
Geet Patel <geetpofficial@gmail.com>
2023-11-01 20:24:59 -0400

    Search API
    - Connected to backend
    - Added pagination

--- backend/services/search.js
+++ backend/services/search.js
@@ -1,13 +1,19 @@
-const books = require("../db/books");
+const db = require("../db");
+const bookService = require("./book")
 
-async function filterBooks(query) {
-    return books.filter((book) => {
-        return (
-            book.title.toLowerCase().includes(query.toLowerCase()) ||
-            book.author.toLowerCase().includes(query.toLowerCase()) ||
-            book.genre.toLowerCase().includes(query.toLowerCase())
-        );
-    });
+async function filterBooks(keywords, page, limit) {
+    const queryStr = `SELECT b.* FROM book b WHERE LOWER(b.title) LIKE '${keywords}%' LIMIT ${limit} OFFSET ${(page - 1) * limit}`;
+    const bookResults = await db.query(queryStr);
+    console.log(bookResults);
+    if (bookResults.rows.length == 0) {
+        return null;
+    }
+    let bookRes = await Promise.all(bookResults.rows.map(async (res) => {
+        let bookRes_ = res;
+        bookRes_.genre = await bookService.getGenres(bookRes_.id);
+        return bookRes_;
+    }));
+    return bookRes;
 }
 
 module.exports = {
