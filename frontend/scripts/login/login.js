8622577a0d02a464ceed6fb930df25e8a108d875
Jhanavi <jhanavibehl@gmail.com>
2023-11-03 03:02:35 -0400

    Create/Update/Login

--- frontend/scripts/login/login.js
+++ frontend/scripts/login/login.js
@@ -1,12 +1,47 @@
+function showPopup() {
+    document.getElementById('customPopup').style.display = 'block';
+  }
+  
+  function closePopup() {
+    document.getElementById('customPopup').style.display = 'none';
+  }
+  
 function validateLogin() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
   
+    const userData = {
+        "user_id":username,
+        "password_hash" : password
+    };
+    console.log(userData);
+    fetch('http://localhost:8000/user/login', {
+      method: 'POST',    
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData)
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log('User logged in:', result);    
+        if(result["User Login"]=='False'){
+            document.getElementById('customPopup').style.display = 'block';
+            document.getElementById('username').value = "";
+            document.getElementById('password').value = "";
+        }
+        else{
+        window.location.href = '../search/search.html';
+        }
+      })  
+      .catch((error) => {
+        console.error('Error loggin in:', error);   
+      });
+/*
     if (username === 'admin' && password === '1234') {
-        window.location.href = '../../templates/search/search.html';
     } else {
         alert('Invalid username or password.');
-    }
+    }*/
 }
 
 function togglePasswordVisibility() {
