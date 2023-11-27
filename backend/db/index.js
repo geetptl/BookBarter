b7467815175e928c79364f6a9b3af8e70cffd370
geet <geetpofficial@gmail.com>
2023-11-27 10:56:22 -0500

    Initing sqlite changes

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,22 +1,60 @@
-const { Pool } = require("pg");
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
 
-const pool = new Pool({
-    connectionString: process.env.POSTGRES_CONN
-});
+const initializeDatabase = (dbPath, ddlScripts) => {
+    const dbExists = fs.existsSync(dbPath);
+    const db = new sqlite3.Database(dbPath, (err) => {
+        if (err) {
+            console.error(err.message);
+            throw err;
+        } else {
+            console.log('Connected to the SQLite database.');
+            if (!dbExists) {
+                console.log('Creating new database schema...');
+                ddlScripts.split(",").map(async scriptPath => {
+                    console.log("Processing", scriptPath);
+                    const ddlScript = fs.readFileSync(scriptPath, 'utf-8');
+                    db.exec(ddlScript, (err) => {
+                        if (err) {
+                            console.error(err.message);
+                            throw err;
+                        } else {
+                            console.log("File", scriptPath, "run successful!");
+                        }
+                    });
+                })
+            }
+        }
+    });
 
-const query = async (text, params) => {
-    const start = Date.now();
-    const res = await pool.query(text, params);
-    const duration = Date.now() - start;
-    if (process.env.VERBOSE == 1) {
-        console.log("executed query", {
-            text,
-            params,
-            duration,
-            rows: res.rowCount,
-        });
-    }
-    return res;
+    return db;
 };
 
-module.exports = { query };
+// Initialize the database
+const db = initializeDatabase(process.env.SQLITE_DB_PATH, process.env.SQLITE_DB_INIT_FILES);
+
+const query = async (text, params) => {
+    return new Promise((resolve, reject) => {
+        const start = Date.now();
+        db.all(text, params, (err, rows) => {
+            const duration = Date.now() - start;
+            if (err) {
+                reject(err);
+            } else {
+                if (process.env.VERBOSE == 1) {
+                    console.log('Executed query', {
+                        text,
+                        params,
+                        duration,
+                        rows: rows.length,
+                    });
+                }
+                resolve(rows);
+            }
+        });
+    });
+};
+
+module.exports = {
+    query
+};
--- backend/scripts/01-ddl.sql
+++ backend/scripts/01-ddl.sql
@@ -1,97 +1,89 @@
 -- Member Table
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY,
+    created_on TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_updated_on TEXT DEFAULT CURRENT_TIMESTAMP,
     user_id TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
     email TEXT NOT NULL UNIQUE,
     phone_number TEXT UNIQUE,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    latitude DECIMAL(10, 6),
-    longitude DECIMAL(10, 6),
+    latitude REAL,
+    longitude REAL,
     is_auth BOOLEAN DEFAULT FALSE
 );
 
 -- Book Table
 CREATE TABLE book (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY,
+    created_on TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_updated_on TEXT DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
     author TEXT NOT NULL,
-    rating DECIMAL(3,2),
+    rating REAL,
     image_url TEXT NOT NULL,
     description TEXT NOT NULL,
     isbn TEXT NOT NULL,
     language TEXT NOT NULL
 );
-CREATE INDEX book_idx_title ON book (title);
 
 -- Genre Table
 CREATE TABLE genre (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY,
+    created_on TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_updated_on TEXT DEFAULT CURRENT_TIMESTAMP,
     name TEXT NOT NULL UNIQUE
 );
 
 -- BookListing Table
-
-CREATE TYPE listing_status AS ENUM ('Available', 'Not_Available');
-
 CREATE TABLE book_listing (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY,
+    created_on TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_updated_on TEXT DEFAULT CURRENT_TIMESTAMP,
     owner_id INTEGER REFERENCES users(id),
     book_id INTEGER REFERENCES book(id),
-    status listing_status,
-    returns_on DATE,
+    status TEXT,
+    returns_on TEXT,
     UNIQUE(owner_id, book_id) 
 );
 
 -- BookGenreMapping Table
 CREATE TABLE book_genre_mapping (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY,
+    created_on TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_updated_on TEXT DEFAULT CURRENT_TIMESTAMP,
     book_id INTEGER REFERENCES book(id),
     genre_id INTEGER REFERENCES genre(id)
 );
 
 -- Session Table
 CREATE TABLE session (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY,
+    created_on TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_updated_on TEXT DEFAULT CURRENT_TIMESTAMP,
     user_id INTEGER REFERENCES users(id),
-    time_to_live timestamp without time zone
+    time_to_live TEXT
 );
-
--- Create an enumerated type for request statuses
-CREATE TYPE request_status AS ENUM ('Pending', 'Accepted', 'Rejected', 'Expired', 'PaymentDeclined');
-
 
 -- Request Table
 CREATE TABLE request (
-    id SERIAL PRIMARY KEY,
-    created timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_modified timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY,
+    created TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_modified TEXT DEFAULT CURRENT_TIMESTAMP,
     borrower_id INTEGER REFERENCES users(id),
     lender_id INTEGER REFERENCES users(id),
     book_listing_id INTEGER REFERENCES book_listing(id),
-    time_to_live timestamp without time zone,
+    time_to_live TEXT,
     borrow_duration INTEGER NOT NULL,
-    status request_status
+    status TEXT
 );
 
 -- ExchangeHistory Table
 CREATE TABLE exchange_history (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY,
+    created_on TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_updated_on TEXT DEFAULT CURRENT_TIMESTAMP,
     lender_id INTEGER REFERENCES users(id),
     borrower_id INTEGER REFERENCES users(id),
     book_id INTEGER REFERENCES book(id),
@@ -100,20 +92,20 @@ CREATE TABLE exchange_history (
 
 -- Payment Table
 CREATE TABLE payment (
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY,
+    created_on TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_updated_on TEXT DEFAULT CURRENT_TIMESTAMP,
     req_id INTEGER REFERENCES request(id),
-    amount DECIMAL(10, 2),
+    amount REAL,
     payment_intent_id TEXT UNIQUE,
     payment_status TEXT
 );
 
--- -- Cards Tables
+-- Cards Table
 CREATE TABLE cards(
-    id SERIAL PRIMARY KEY,
-    created_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    last_updated_on timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    id INTEGER PRIMARY KEY,
+    created_on TEXT DEFAULT CURRENT_TIMESTAMP,
+    last_updated_on TEXT DEFAULT CURRENT_TIMESTAMP,
     payer_id INTEGER REFERENCES users(id),
     stripe_customer_id TEXT UNIQUE,
     payment_method_id TEXT UNIQUE
